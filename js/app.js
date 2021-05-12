0427c7f1f9efac73424cabbcd26e4a61b1c8b7a0
irina694 <patrikeeva.irina@gmail.com>
2021-05-12 14:44:10 -0700

    Added navigation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,56 @@
 /* JavaScript for Portfolio */
 
-function showGallery(input) {
+function showGallery(id) {
 
-  console.log('show gallery');
-  console.log(input);
-  
+  if ( id && document ) {
+
+    // Highlight selected navigation
+
+    var navs = document.getElementsByClassName('nav');
+    
+    // Remove active class from all navs except for selected nav
+    if ( navs ) {
+
+      for ( var i = 0; i < navs.length; i++ ) {
+
+          var nav = navs[i];
+          if ( nav && nav.classList ) {
+
+            if ( nav.id === id ) {
+            // Active nav
+              nav.classList.remove('text-gray-500');
+              nav.classList.add('text-gray-800');
+              nav.classList.remove('cursor-pointer');
+            } else {
+              // Inactive nav
+              nav.classList.remove('text-gray-800');
+              nav.classList.add('text-gray-500');
+              nav.classList.add('hover:text-gray-800');
+              nav.classList.add('cursor-pointer');
+            }
+          }
+      }
+    }
+
+    // Show selected gallery
+
+    // Hide all galleries
+    var galleries = document.getElementsByClassName('gallery');
+    if ( galleries ) {
+      for (var i = 0; i < galleries.length; i++ ) {
+        var gal = galleries[i];
+        if ( gal && gal.classList ) {
+          gal.classList.add('hidden');
+        }
+      }
+    }
+
+    // Show selected gallery
+    var galleryId = id + '-gallery';
+    var gallery = document.getElementById(galleryId);
+    if ( gallery && gallery.classList ) {
+      gallery.classList.remove('hidden');
+    }
+
+  }
 }
